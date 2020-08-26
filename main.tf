e70e091737fadb882e47afb38330504f50095e56
dschuler36 <dschuler36@gmail.com>
2020-08-26 17:56:22 +0000

    updated tag

--- main.tf
+++ main.tf
@@ -91,7 +91,7 @@ resource "azurerm_virtual_machine" "catapp" {
   resource_group_name = azurerm_resource_group.myresourcegroup.name
   vm_size             = var.vm_size
 
-  freeform_tags = {
+  tags = {
     Billable = "true"
     Department = "devops"
   }
